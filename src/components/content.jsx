7a740b2372238374ff938a124732b176f6304eba
Cesar Torres <catq2596@gmail.com>
2020-10-16 10:17:31 -0700

    Estructura base

---
+++ src/components/content.jsx
@@ -0,0 +1,13 @@
+import { render } from "@testing-library/react";
+import React, { Component } from "react";
+import "./css/content.css";
+
+class Content extends Component {
+    render() {
+        return (
+            <div class="content">
+                <h3>Aqui va Contenido</h3>
+            </div>
+        );
+    }
+}
