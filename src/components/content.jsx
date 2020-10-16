14ca83f8c0418f62a1a4ef320ce286929325636e
Shamira03 <macias3roa@gmail.com>
2020-10-16 12:33:46 -0700

    Prueba de subir archivos a Amazon

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -1,14 +1,46 @@
 import { render } from "@testing-library/react";
 import React, { Component } from "react";
 import "./css/content.css";
+import ReactS3 from 'react-s3';
+import {aws} from '../../keys';
+
+const config = {
+    bucketName: 'textract-console-us-west-2-51755281-cab5-4f5e-9891-0dad1c49f1b6',
+    dirName: 'pdf',
+    region: 'us-west-2',
+    accessKeyId: aws.accessKeyId,
+    secretAccessKey: aws.secretAccessKey,
+}
 
 class Content extends Component {
+   
+    constructor()
+    {
+        super();
+    }
+   
+    upload (e){
+        console.log(e.target.files[0]);
+        ReactS3.upload(e.target.files[0], config)
+        .then ((data) => {
+            console.log(data);
+        }) 
+        .catch( (err)=>{
+            alert(err);
+        }
+        )
+    }
     render() {
         return (
-            <div class="content">
-                <h3>Aqui va Contenido</h3>
+            <div>
+
+                <h3>Cargar archivo</h3>
+                <input 
+                    type = 'file' 
+                    onChange = {this.upload}
+                />
             </div>
-        );
+        )
     }
 }
 export default Content;
