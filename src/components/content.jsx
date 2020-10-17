ab0b4fc826aa0b1e7e655a55b58d906250d7b712
Shamira03 <macias3roa@gmail.com>
2020-10-16 19:47:48 -0700

    Intento antiguo

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -3,12 +3,13 @@ import React, { Component } from "react";
 import "./css/content.css";
 import ReactS3 from 'react-s3';
 
+
 const config = {
     bucketName: 'textract-console-us-west-2-51755281-cab5-4f5e-9891-0dad1c49f1b6',
     dirName: 'pdf',
     region: 'us-west-2',
-    accessKeyId: 'AKIA2RK7O5A44SQKPKUE',
-    secretAccessKey: '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',
+    accessKeyId: '',
+    secretAccessKey: '',
 }
 
 class Content extends Component {
@@ -18,27 +19,29 @@ class Content extends Component {
         super();
     }
    
-    upload (e){
+    upload(e){
         console.log(e.target.files[0]);
         ReactS3.upload(e.target.files[0], config)
         .then ((data) => {
-            console.log(data);
+            console.log(data.location);
         }) 
         .catch( (err)=>{
             alert(err);
-        }
-        )
+        })
     }
+
     render() {
         return (
             <div>
 
-                <h3>Cargar archivo</h3>
+                <h3 id = "myHeader">Cargar archivo</h3>
                 <input 
                     type = 'file' 
                     onChange = {this.upload}
                 />
+                
             </div>
+                
         )
     }
 }
