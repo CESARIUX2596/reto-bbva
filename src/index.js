b8d42f4495c63d87d66a4aafba30dd0ed49dcd30
Cesar Torres <catq2596@gmail.com>
2020-10-17 22:41:49 -0700

    IDK but it works

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 // import Content from "./components/content";
-// import Tablas from "./components/tablas";
-import Proyecto from "./components/proyecto";
+import Tablas from "./components/tablas";
+// import Proyecto from "./components/proyecto";
 
 ReactDOM.render(
 	<div>
 		{/* <Content/> */}
-		{/* <Tablas/> */}
-		<Proyecto/>
+		<Tablas/>
+		{/* <Proyecto/> */}
 	</div>,
 	document.getElementById("root")
 );
